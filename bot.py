c51c709cc0a19314a85d568693ebbc7882bf228d
Edward Paget <edward@cassetteta.pe>
2017-10-20 16:27:47 -0400

    Handle welcome request

--- bot.py
+++ bot.py
@@ -18,44 +18,74 @@ except:
         MESSAGE = 'Manually set the Message if youre not running through heroku or have not set vars in ENV'
         TOKEN = 'Manually set the API Token if youre not running through heroku or have not set vars in ENV'
         UNFURL = 'FALSE'
-###############################################################
+        ###############################################################
+
 
 def parse_join(message):
-    m = json.loads(message)
-    if m['type'] == "team_join" or \
-       (m['type'] == 'message' and m['text'] == '!welcome'):
-        x = requests.get("https://slack.com/api/im.open?token="+TOKEN+"&user="+m["user"]["id"])
-        x = x.json()
-        x = x["channel"]["id"]
-        if (UNFURL.lower() == "false"):
-          xx = requests.post("https://slack.com/api/chat.postMessage?token="+TOKEN+"&channel="+x+"&text="+urllib.quote(MESSAGE)+"&parse=full&as_user=true&unfurl_links=false")
-        else:
-          xx = requests.post("https://slack.com/api/chat.postMessage?token="+TOKEN+"&channel="+x+"&text="+urllib.quote(MESSAGE)+"&parse=full&as_user=true")
+        m = json.loads(message)
+        if m['type'] == "team_join":
+                x = requests.get(
+                        "https://slack.com/api/im.open",
+                        params=dict(
+                                token=TOKEN,
+                                user=m["user"]["id"]
+                        )
+                )
+                x = x.json()
+                x = x["channel"]["id"]
+                xx = requests.post(
+                        "https://slack.com/api/chat.postMessage",
+                        data=dict(
+                                token=TOKEN
+                                channel=x,
+                                text=urllib.quote(MESSAGE),
+                                parse="full",
+                                as_user=True,
+                                unfurl_links=(UNFURL.lower() == "true")
+                        )
+                )
+        elif m['type'] == 'message' and m['text'] == '!welcome':
+                x = m["channel"]
+                xx = requests.post(
+                        "https://slack.com/api/chat.postMessage",
+                        data=dict(
+                                token=TOKEN
+                                channel=x,
+                                text=urllib.quote(MESSAGE),
+                                parse="full",
+                                as_user=True,
+                                unfurl_links=(UNFURL.lower() == "true")
+                        )
+                )
+
 
 #Connects to Slacks and initiates socket handshake
 def start_rtm():
-    r = requests.get("https://slack.com/api/rtm.start?token="+TOKEN, verify=False)
-    r = r.json()
-    print r
-    r = r["url"]
+        r = requests.get("https://slack.com/api/rtm.start?token="+TOKEN, verify=False)
+        r = r.json()
+        print r
+        r = r["url"]
     return r
 
+
 def on_message(ws, message):
-    parse_join(message)
+        parse_join(message)
+
 
 def on_error(ws, error):
-    print "SOME ERROR HAS HAPPENED", error
+        print "SOME ERROR HAS HAPPENED", error
+
 
 def on_close(ws):
-    print '\033[91m'+"Connection Closed"+'\033[0m'
+        print '\033[91m'+"Connection Closed"+'\033[0m'
+
 
 def on_open(ws):
-    print "Connection Started - Auto Greeting new joiners to the network"
+        print "Connection Started - Auto Greeting new joiners to the network"
 
 
 if __name__ == "__main__":
-    r = start_rtm()
-    ws = websocket.WebSocketApp(r, on_message = on_message, on_error = on_error, on_close = on_close)
-    #ws.on_open
+        r = start_rtm()
+        ws = websocket.WebSocketApp(r, on_message = on_message, on_error = on_error, on_close = on_close)
+        #ws.on_open
     ws.run_forever()
-
